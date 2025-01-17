3aab98ea6ecf90d2a6f5e317474a89174ea9db9d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-17 20:07:26 +0200

    Add and setup express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import { engine } from "express-handlebars";
 
 const app = express();
 
-app.get("/", (req, res) => { return res.send("It Works!!!"); });
+app.engine("hbs", engine({
+    extname: "hbs"
+}));
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+
+app.get("/", (req, res) => { return res.render("home", { layout: false }); });
 
 app.listen(3000, console.log("Server is listening on port: 3000..."));
