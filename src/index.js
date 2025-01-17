df952886838caec5a0ed5814b7fac35bbaef4eeb
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-17 19:58:56 +0200

    Install and setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("It Works!!!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => { return res.send("It Works!!!"); });
+
+app.listen(3000, console.log("Server is listening on port: 3000..."));
