0c0f69654c567b154f357733d5a652ff41e23d26
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-18 20:36:12 +0200

    Add search by genre and year

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,8 +3,13 @@ import { v4 as uuid } from "uuid";
 
 export default {
     getAll(filter = {}) {
-        if (filter.search) return movies.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
-        return movies;
+        let result = [...movies];
+
+        if (filter.search) result = [...result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))];
+        if (filter.genre) result = [...result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase())];
+        if (filter.year) result = [...result.filter(movie => movie.year === +filter.year)];
+
+        return result;
     },
     findOne(id) {
         const movie = movies.find(movie => movie.id === id);
