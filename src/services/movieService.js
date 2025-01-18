1a1a46e21a75ef2a9ee0a05c0b2f7e9dc56acce2
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-18 20:01:52 +0200

    Update findOne in movieService

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,9 +4,8 @@ import { v4 as uuid } from "uuid";
 export default {
     getAll() { return movies },
     findOne(id) {
-        let movie = movies.find(movie => movie.id === id), rating = '';
-        for (let i = 0; i < Math.round(movie.rating); i++) rating += "★"
-        movie.rating = rating;
+        const movie = movies.find(movie => movie.id === id);
+        movie.rating = "★".repeat(Math.round(movie.rating));
         return movie;
     },
     create(data) {
