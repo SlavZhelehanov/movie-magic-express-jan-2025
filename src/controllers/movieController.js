cea2cc0e174024c7c794b5986360f923d06b3387
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-18 17:57:07 +0200

    Add Details Page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import movies from "../movieDB.js";
 
 const movieController = Router();
 
 movieController.get("/create", (req, res) => { return res.render("create", { title: "Create" }); });
 
 movieController.get("/about", (req, res) => { return res.render("about", { title: "About" }); });
+
+movieController.get("/:id/details", (req, res) => { return res.render("details", { title: "Details", movie: movies.find(id => id === req.params.id) }); });
 
 export default movieController;
