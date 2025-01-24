7960df1e2502265929c43290d17f72773c24f24d
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-24 19:01:36 +0200

    Config handlebars to use mongo documents

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ const DB_URI = "mongodb://localhost:27017/movie-magic-jan2015"
 
 try {
     await mongoose.connect(DB_URI);
-    console.log("DB is connected");    
+    console.log("DB is connected");
 } catch (dbConnectionErr) {
-    console.error("DB connection error: ", dbConnectionErr.message);    
+    console.error("DB connection error: ", dbConnectionErr.message);
 }
 
 // app.use("/static", express.static("src/public"));
@@ -25,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
 app.engine("hbs", engine({
-    extname: "hbs"
+    extname: "hbs",
+    runtimeOptions: { allowProtoPropertiesByDefault: true } // Instead adding .lean() after calling mongoose models
 }));
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
