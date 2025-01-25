69099f95302f30a963c0d8184a978c8fa43badce
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-25 21:25:07 +0200

    Refactor movie create

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,8 +6,8 @@ const movieController = Router();
 // CREATE
 movieController.get("/create", (req, res) => { return res.render("create", { title: "Create" }); });
 
-movieController.post("/create", (req, res) => {
-    movieService.create({ ...req.body, year: +req.body.year, rating: +req.body.rating });
+movieController.post("/create", async (req, res) => {
+    await movieService.create({ ...req.body, year: +req.body.year, rating: +req.body.rating });
     return res.redirect("/");
 });
 
