bd7c41cc04b235bba1db9b17b82bae586056e815
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-02-03 13:18:31 +0200

    Add register page and fix about page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,4 +2,7 @@ import { Router } from "express";
 
 const authController = Router();
 
+// REGISTER
+authController.get("/register", (req, res) => { return res.render("auth/register", { title: "Register" }) });
+
 export default authController;
