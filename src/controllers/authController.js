e24350e0f84da6e3851ec268cabad06fe560b138
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-02-03 13:26:45 +0200

    Send data to register endpoint

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,4 +5,14 @@ const authController = Router();
 // REGISTER
 authController.get("/register", (req, res) => { return res.render("auth/register", { title: "Register" }) });
 
+authController.post("/register", async (req, res) => {
+    let { email, password, confirm } = req.body;
+    email = email.trim(); password = password.trim(); confirm = confirm.trim();
+
+    console.log(`Email: ${email}; Password: ${password}; Repeat-password: ${confirm}`);
+
+    return res.end();
+    return res.render("auth/register", { title: "Register" })
+});
+
 export default authController;
